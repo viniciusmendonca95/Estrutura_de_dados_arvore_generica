da36aa51aeaf6352c0465d0b7795e3aeba19c8a6
Luís Felipe Monteiro <felipemonteirose@gmail.com>
2022-08-29 22:30:55 -0300

    Altura e Profundidade

--- src/main/java/com/mycompany/arvore_generica/No.java
+++ src/main/java/com/mycompany/arvore_generica/No.java
@@ -7,8 +7,8 @@ package com.mycompany.arvore_generica;
 
 import java.util.ArrayList;
 import java.util.List;
+
 /**
- *
  * @author vinyj
  */
 public class No {
@@ -16,33 +16,31 @@ public class No {
     private int valor;
     private List<No> filhos;
     private int nivel;
-    
+
     //Construtor do nó
-    public No(int valor){
+    public No(int valor) {
         this.valor = valor;
         this.filhos = new ArrayList<>();
-        this.nivel = nivel;
     }
-    
+
     //Método de adição ode filho ao nó
-    public No adicionarFilhoAoNo(No no){
+    public void adicionarFilhoAoNo(No no) {
         no.nivel = nivel + 1;
         filhos.add(no);
-        return no;
     }
-  
+
     //Método de retorno do valor do nó
     public int getValorNo() {
         return valor;
     }
-    
+
     //Método de retorno da lista de filhos do nó
     public List<No> getFilhosNo() {
         return filhos;
     }
-    
+
     //Método de retorno do nível do nó
     public int getNivelNo() {
         return nivel;
-    }  
+    }
 }
